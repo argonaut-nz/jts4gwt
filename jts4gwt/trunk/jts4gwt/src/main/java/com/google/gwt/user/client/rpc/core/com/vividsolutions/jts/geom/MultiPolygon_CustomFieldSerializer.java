77699673e9ca665671ad15394403f45e0ae35766
pretyman <pretyman@2db769d5-2891-4024-b92c-db9f4334d7b2>
2011-06-12 18:24:44 +0000

    Added Delaunay Triangulation
    Fixed a bug in the MultiPolygon_CustomFieldSerializer which I don't know how it didn't show up before

--- jts4gwt/trunk/jts4gwt/src/main/java/com/google/gwt/user/client/rpc/core/com/vividsolutions/jts/geom/MultiPolygon_CustomFieldSerializer.java
+++ jts4gwt/trunk/jts4gwt/src/main/java/com/google/gwt/user/client/rpc/core/com/vividsolutions/jts/geom/MultiPolygon_CustomFieldSerializer.java
@@ -3,7 +3,11 @@ package com.google.gwt.user.client.rpc.core.com.vividsolutions.jts.geom;
 import com.google.gwt.user.client.rpc.SerializationException;
 import com.google.gwt.user.client.rpc.SerializationStreamReader;
 import com.google.gwt.user.client.rpc.SerializationStreamWriter;
-import com.vividsolutions.jts.geom.*;
+import com.vividsolutions.jts.geom.Geometry;
+import com.vividsolutions.jts.geom.GeometryFactory;
+import com.vividsolutions.jts.geom.MultiPolygon;
+import com.vividsolutions.jts.geom.Polygon;
+import com.vividsolutions.jts.geom.PrecisionModel;
 
 public final class MultiPolygon_CustomFieldSerializer 
 {
@@ -26,7 +30,7 @@ public final class MultiPolygon_CustomFieldSerializer
 	}
 
 	public static void serialize(SerializationStreamWriter streamWriter,
-			MultiLineString instance) throws SerializationException
+			MultiPolygon instance) throws SerializationException
     {
 		streamWriter.writeInt(instance.getSRID());
         streamWriter.writeInt(instance.getNumGeometries());
